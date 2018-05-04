b77d88b6c5409b4605d96c4fa132488a51e523fb
Oneal Marange <Onealm@afrosoft.co.zw>
2018-05-04 18:55:28 +0200

    commit home implementations

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import * as firebase from 'firebase';
+import { MainPage } from '../pages/main/main';
+
+const config = {
+  apiKey: "AIzaSyAwp7I3aM5ENEu1gITU_yUk6oNwLTHmAvQ",
+  authDomain: "land-manager.firebaseapp.com",
+  databaseURL: "https://land-manager.firebaseio.com",
+  projectId: "land-manager",
+  storageBucket: "land-manager.appspot.com",
+  messagingSenderId: "478647188127"
+};
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+
+export class MyApp {
+  rootPage: any = MainPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+    firebase.initializeApp(config);
+  }
+}
---
+++ src/pages/main/main.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the MainPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-main',
+  templateUrl: 'main.html',
+})
+export class MainPage {
+  private rootPage;
+  private homePage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.rootPage = HomePage;
+    this.homePage = HomePage;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MainPage');
+  }
+
+  openPage(p) {
+    this.rootPage = p;
+  }
+
+}
